4fbe55f83cc135963a48c7820738fef1ea0b6606
Javier <jagi.ccs@gmail.com>
2020-03-03 14:18:37 +0000

    authentication almost done

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :pictures
-  resources :users
+  # resources :pictures
+  # resources :users
+  post "/sign-in", to: "users#sign_in"
+  get "/validate", to: "users#validate"
+  # get "/items", to: "users#items"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
